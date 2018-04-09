a3f811eb22845aa408d96f02c411e3bc3fc16789
Pedro Rabinovitch <pedromr@gmail.com>
2018-04-09 12:33:53 -0400

    New game with options. Expanding by clicking on revealed tiles

--- src/mines.jsx
+++ src/mines.jsx
@@ -43,6 +43,34 @@ class Board extends React.Component {
 class ControlPanel extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            choosingGameOptions: false,
+            x: props.config.x,
+            y: props.config.y,
+            mines: props.config.mines,
+        }
+    }
+
+    handleXChanged(val) {
+        this.setState({x: val});
+    }
+
+    handleYChanged(val) {
+        this.setState({y: val});
+    }
+
+    handleMinesChanged(val) {
+        this.setState({mines: val});
+    }
+
+    onNewGameButton() {
+        this.setState({choosingGameOptions: true});
+    }
+
+    onCreateGameButton() {
+        this.setState({choosingGameOptions: false});
+        this.props.onNewGame(this.state);
     }
 
     onClickFlag() {
@@ -51,8 +79,23 @@ class ControlPanel extends React.Component {
     }
 
     render() {
-        return <div id="#controlPanel">
-            <input type="checkbox" onClick={()=>this.onClickFlag()} />Placing Flag
+        const mode = this.props.options.placingFlag ? "Placing Flag" : "Peeking";
+        const config = this.props.config;
+
+        const newGame = this.state.choosingGameOptions ?
+            <div id="newGameOpt"> 
+                New game<br/>
+                Rows: <input type="text" defaultValue={config.y} onChange={(e)=>this.handleYChanged(e.target.value)}/><br/>
+                Columns: <input type="text" defaultValue={config.x} onChange={(e)=>this.handleXChanged(e.target.value)}/><br/>
+                Mines: <input type="text" defaultValue={config.mines} onChange={(e)=>this.handleMinesChanged(e.target.value)}/><br/>
+                <button onClick={()=>this.onCreateGameButton()}>Create Game</button>
+            </div> 
+            : <button onClick={()=>this.onNewGameButton()}>New Game</button>;
+
+        return <div id="controlPanel">
+            <button onClick={()=>this.onClickFlag()}>Change Mode</button> {mode} <br/>
+            {config.x}x{config.y}, {config.mines} mines<br/><br/><br/>
+            {newGame}
         </div>;
     }
 }
@@ -64,47 +107,104 @@ class Game extends React.Component {
         let cols = 10;
         let rows = 6;
         let nMines = 8;
-        this.state = {}
-        this.state.options = { placingFlag: false };
-        this.state.config = {x: cols, y: rows};
-        this.state.seen = Array(cols*rows).fill(false);
-        this.state.around = Array(cols*rows).fill(0);
-        this.state.mines = Array(cols*rows).fill(false);
-        this.state.flags = Array(cols*rows).fill(false);
+        let config = {x: cols, y: rows, mines: nMines};
+        this.createGame(config);
+    }
+
+    createGame(config) {
+        let cols = config.x;
+        let rows = config.y;
+        config.mines = Math.min(config.mines, cols * rows);
+
+        let nMines = config.mines;
+
+        let state = {}
+        state.options = { placingFlag: false };
+        state.config = {x: cols, y: rows, mines: nMines};
+        state.seen = Array(cols*rows).fill(false);
+        state.around = Array(cols*rows).fill(0);
+        state.mines = Array(cols*rows).fill(false);
+        state.flags = Array(cols*rows).fill(false);
 
         while(nMines > 0) {
             let randX = getRandomInt(cols);
             let randY = getRandomInt(rows);
             let pos = randX+cols*randY;
-            if (!this.state.mines[pos]) {
+            if (!state.mines[pos]) {
                 nMines--;
-                this.state.mines[pos] = true;
+                state.mines[pos] = true;
                 for (let dy=-1; dy <= 1; dy++) {
-                    if (randY + dy < 0 || randY + dy >= cols) continue;
+                    if (randY + dy < 0 || randY + dy >= rows) continue;
                     for (let dx=-1; dx <= 1; dx++) {
                         if (randX + dx < 0 || randX + dx >= cols) continue;                        
                         let neighborPos =  pos + dx + dy*cols;
-                        this.state.around[neighborPos]++;
+                        state.around[neighborPos]++;
                     }
                 }
             }
         }
+
+        if (this.state)
+            this.setState(state);
+        else
+            this.state = state;
     }
 
     handleClick(x, y) {
         const pos = x + y * this.state.config.x;
+        let seen = this.state.seen.slice();
 
-        if (this.state.options.placingFlag) {
-            let flags = this.state.flags.slice();
-            flags[pos] = !flags[pos];
-            this.setState({flags: flags});
-        } else {
-            let seen = this.state.seen.slice();
-            if (!seen[pos]) {
+        if (!seen[pos]) {
+            if (this.state.options.placingFlag) {
+                let flags = this.state.flags.slice();
+                flags[pos] = !flags[pos];
+                this.setState({flags: flags});
+            } else {
                 seen[pos] = true;
                 this.setState({seen: seen});
             }
+        } else {
+            //expand
+            var flagsAround = this.countFlagsAround(x, y);
+            if (flagsAround == this.state.around[pos]) {
+                this.expandAround(x,y);
+            }
         }
+    }
+
+    countFlagsAround(x, y) {
+        let n = 0;
+        let cols = this.state.config.x;
+        let rows = this.state.config.y;
+
+        for (let dy=-1; dy <= 1; dy++) {
+            if (y + dy < 0 || y + dy >= rows) continue;
+            for (let dx=-1; dx <= 1; dx++) {
+                if (x + dx < 0 || x + dx >= cols) continue;                        
+                let neighborPos =  x + dx + (y+dy)*cols;
+                if (this.state.flags[neighborPos])
+                    n++;
+            }
+        }
+
+        return n;
+    }
+
+    expandAround(x, y) {
+        let cols = this.state.config.x;
+        let rows = this.state.config.y;
+        let seen = this.state.seen.slice();        
+
+        for (let dy=-1; dy <= 1; dy++) {
+            if (y + dy < 0 || y + dy >= rows) continue;
+            for (let dx=-1; dx <= 1; dx++) {
+                if (x + dx < 0 || x + dx >= cols) continue;                        
+                let neighborPos =  x + dx + (y+dy)*cols;
+                if (!this.state.flags[neighborPos])
+                    seen[neighborPos] = true;
+            }
+        }
+        this.setState({seen: seen});
     }
 
     handleOptionsChanged(newOptions) {
@@ -124,7 +224,9 @@ class Game extends React.Component {
              />
              <ControlPanel
              options={this.state.options}
+             config={this.state.config}
              onChange={(newOptions) => this.handleOptionsChanged(newOptions)}
+             onNewGame={(c) => this.createGame(c)}
               />
             </div>;
     }
