30a9940990ae8209faa41710d96221652056a470
Pedro Rabinovitch <pedromr@gmail.com>
2018-05-09 13:46:18 -0400

    Minor tweaks

--- src/conf/StoryDatabase.js
+++ src/conf/StoryDatabase.js
@@ -3,9 +3,9 @@ import * as Require from '../meta/Require';
 
 const allStory = [
     MakeLine('welcome',
-    'Congratulations on completing your basic training. You will now be granted access to your console from where you will undertake real, live, on-the-field ground weaponry defusal and disposal. Your squad awaits your orders.'
+    'Congratulations on completing your basic training. You will now be granted access to your personal console. Use it well.'
     ),
-    MakeLine('first-play',"Show us what you've got, rookie.", Require.OnScreen(types.SCREEN_PLAY_MINES)), 
+    MakeLine('first-play',"This is a simplified view of the weaponry you must defuse and dispose of. Your squad awaits your orders. Show us what you've got, rookie.", Require.OnScreen(types.SCREEN_PLAY_MINES)), 
     MakeReference('first-results',"As you complete missions you'll get Credits you can use to upgrade your monitoring station and your squad's equipment."), 
     MakeLine('no-credits', 'Tough luck. Try again so we can offer you more to learn and do.',[Require.OnScreen(types.SCREEN_MAIN), Require.Not(Require.HasFeature(types.FEATURE_SHOW_SHOP))]),
     MakeLine('shop-available',"I'll now unlock the Shop functionality so you can invest on upgrades.", [Require.OnScreen(types.SCREEN_MAIN), Require.HasFeature(types.FEATURE_SHOW_SHOP)]),
